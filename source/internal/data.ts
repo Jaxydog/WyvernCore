b49211af322a2b015eca2cf0b2fb32f385c56891
jaxydog <32988901+jaxydog@users.noreply.github.com>
2022-09-29 20:12:24 -0400

    Fix data get condition

--- source/internal/data.ts
+++ source/internal/data.ts
@@ -613,7 +613,7 @@ export class DualStorage extends AsyncStorage {
 	}
 	public async get<T = unknown>(key: string, settings = DEFAULT_REQUEST_SETTINGS) {
 		const cache = this.__cache.get<T>(key, settings)
-		const files = cache !== undefined ? await this.__files.get<T>(key, settings) : undefined
+		const files = cache === undefined ? await this.__files.get<T>(key, settings) : undefined
 		if (cache === undefined && files !== undefined) this.__cache.set<T>(key, files, settings)
 		return (cache ?? files) as T | undefined
 	}
